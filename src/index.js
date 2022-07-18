365174d1bba6e85ee41d14974cc607d546241a74
romeonorse <romeonorse@gmail.com>
2022-07-18 21:38:07 +0300

    Add description

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Project, createObject} from './js/defaultProject';
+import {createObject} from './js/createObject';
 
 const title = document.querySelector('#title');
 const desc = document.querySelector('#desc');
@@ -6,8 +6,16 @@ const date = document.querySelector('#date');
 
 // const projects = document.querySelector('.projects');
 const btn = document.querySelector('.btn');
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(e) {
   createObject(title.value, desc.value, date);
 })
 
+const todos = document.querySelectorAll('.todo');
+const descs = document.querySelectorAll('.desc');
+todos.forEach(todo => todo.addEventListener('click', function(e) {
+  // const desc = descs.find(desc => desc.innerText.contains(e.target.innerText));
+  // desc.classList.remove('hidden');
+  console.log(todo.innerText);
+}))
 
+console.log(todos);
---
+++ src/js/createObject.js
@@ -0,0 +1,30 @@
+class Project {
+  constructor(title, desc, dueDate) {
+    this.title = title,
+    this.desc = desc,
+    this.dueDate = dueDate
+  }
+}
+
+function createObject(title, desc, dueDate) {
+  // Creating new todo
+  const newProject = new Project(title, desc, dueDate);
+  const newTodo = document.createElement('div');
+  newTodo.classList.add('todo');
+  newTodo.innerHTML = `
+    <h3>${title}</h3>
+  `;
+  const projects = document.querySelector('.projects');
+  projects.appendChild(newTodo);
+
+  // Creating description
+  const newDesc = document.createElement('div');
+  newDesc.innerHTML = `
+  <h3>Todo name: ${desc}</h3>
+  <h4>Todo date: ${dueDate.value}</h4>
+  `;
+  const description = document.querySelector('.project-info');
+  description.appendChild(newDesc);
+}
+
+export {createObject};
